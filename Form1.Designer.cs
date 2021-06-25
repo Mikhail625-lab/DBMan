e4d16c056c4a5e3d03b7b1ecb3a6d41bac6c458a
Robot NUC <Robot.Smart@protonmail.com>
2021-06-25 14:47:40 +0300

    ---

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -60,90 +60,91 @@ namespace DBMan
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 249);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(386, 271);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(225, 23);
+            this.textBox1.Size = new System.Drawing.Size(227, 23);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(63, 225);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(386, 247);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(225, 23);
+            this.textBox2.Size = new System.Drawing.Size(227, 23);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(63, 201);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Location = new System.Drawing.Point(386, 223);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(225, 23);
+            this.textBox3.Size = new System.Drawing.Size(227, 23);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(63, 177);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Location = new System.Drawing.Point(386, 199);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(225, 23);
+            this.textBox4.Size = new System.Drawing.Size(227, 23);
             this.textBox4.TabIndex = 3;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(63, 153);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Location = new System.Drawing.Point(386, 175);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(225, 23);
+            this.textBox5.Size = new System.Drawing.Size(227, 23);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(63, 129);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Location = new System.Drawing.Point(386, 151);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(225, 23);
+            this.textBox6.PasswordChar = '*';
+            this.textBox6.Size = new System.Drawing.Size(227, 23);
             this.textBox6.TabIndex = 5;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(63, 105);
-            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Location = new System.Drawing.Point(386, 127);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(225, 23);
+            this.textBox7.Size = new System.Drawing.Size(227, 23);
             this.textBox7.TabIndex = 6;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(63, 81);
-            this.textBox8.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox8.Location = new System.Drawing.Point(386, 103);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(225, 23);
+            this.textBox8.Size = new System.Drawing.Size(227, 23);
             this.textBox8.TabIndex = 7;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(63, 57);
-            this.textBox9.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox9.Location = new System.Drawing.Point(386, 79);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(225, 23);
+            this.textBox9.Size = new System.Drawing.Size(138, 23);
             this.textBox9.TabIndex = 8;
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(63, 33);
-            this.textBox10.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox10.Location = new System.Drawing.Point(386, 55);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(225, 23);
+            this.textBox10.Size = new System.Drawing.Size(138, 23);
             this.textBox10.TabIndex = 9;
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(63, 9);
-            this.textBox11.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox11.Location = new System.Drawing.Point(386, 31);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(225, 23);
+            this.textBox11.Size = new System.Drawing.Size(227, 23);
             this.textBox11.TabIndex = 10;
             this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
             // 
@@ -157,80 +158,90 @@ namespace DBMan
             "MariaDB",
             "Oracle",
             "PostgreSQL",
-            "MongoDB"});
-            this.comboBox1.Location = new System.Drawing.Point(292, 9);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            "MongoDB",
+            "FireBird"});
+            this.comboBox1.Location = new System.Drawing.Point(615, 31);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(225, 23);
+            this.comboBox1.Size = new System.Drawing.Size(227, 23);
             this.comboBox1.TabIndex = 11;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Location = new System.Drawing.Point(-147, 31);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(70, 15);
             this.label1.TabIndex = 12;
-            this.label1.Text = "label1";
+            this.label1.Text = "name conn:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Location = new System.Drawing.Point(272, 55);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(33, 15);
             this.label2.TabIndex = 13;
-            this.label2.Text = "label2";
+            this.label2.Text = "host:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 57);
+            this.label3.Location = new System.Drawing.Point(272, 79);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.Size = new System.Drawing.Size(32, 15);
             this.label3.TabIndex = 14;
-            this.label3.Text = "label3";
+            this.label3.Text = "port:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 81);
+            this.label4.Location = new System.Drawing.Point(272, 103);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(73, 15);
             this.label4.TabIndex = 15;
-            this.label4.Text = "label4";
+            this.label4.Text = "data base(s):";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 105);
+            this.label5.Location = new System.Drawing.Point(272, 127);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.Size = new System.Drawing.Size(32, 15);
             this.label5.TabIndex = 16;
-            this.label5.Text = "label5";
+            this.label5.Text = "user:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 129);
+            this.label6.Location = new System.Drawing.Point(272, 151);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 15);
+            this.label6.Size = new System.Drawing.Size(60, 15);
             this.label6.TabIndex = 17;
-            this.label6.Text = "label6";
+            this.label6.Text = "password:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 153);
+            this.label7.Location = new System.Drawing.Point(272, 175);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(38, 15);
@@ -240,7 +251,7 @@ namespace DBMan
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 177);
+            this.label8.Location = new System.Drawing.Point(272, 199);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(38, 15);
@@ -250,7 +261,7 @@ namespace DBMan
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 201);
+            this.label9.Location = new System.Drawing.Point(272, 223);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(38, 15);
@@ -260,7 +271,7 @@ namespace DBMan
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(6, 225);
+            this.label10.Location = new System.Drawing.Point(272, 247);
             this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(44, 15);
@@ -270,12 +281,13 @@ namespace DBMan
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(6, 249);
+            this.label11.Location = new System.Drawing.Point(272, 271);
             this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(44, 15);
+            this.label11.Size = new System.Drawing.Size(67, 15);
             this.label11.TabIndex = 22;
-            this.label11.Text = "label11";
+            this.label11.Text = "comments:";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // button1
             // 
@@ -291,16 +303,16 @@ namespace DBMan
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(292, 33);
+            this.listBox1.Location = new System.Drawing.Point(615, 55);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(225, 274);
+            this.listBox1.Size = new System.Drawing.Size(227, 274);
             this.listBox1.TabIndex = 24;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(63, 277);
+            this.button2.Location = new System.Drawing.Point(386, 298);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(225, 23);
+            this.button2.Size = new System.Drawing.Size(226, 23);
             this.button2.TabIndex = 25;
             this.button2.Text = "set default var";
             this.button2.UseVisualStyleBackColor = true;
@@ -309,9 +321,9 @@ namespace DBMan
             // textBox12
             // 
             this.textBox12.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.textBox12.Location = new System.Drawing.Point(63, 309);
+            this.textBox12.Location = new System.Drawing.Point(386, 331);
             this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(454, 23);
+            this.textBox12.Size = new System.Drawing.Size(456, 23);
             this.textBox12.TabIndex = 26;
             // 
             // frmConn1
@@ -319,7 +331,7 @@ namespace DBMan
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1264, 381);
+            this.ClientSize = new System.Drawing.Size(845, 368);
             this.Controls.Add(this.textBox12);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
@@ -347,7 +359,7 @@ namespace DBMan
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmConn1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿/*
+ver: 0.1a  date: 2021.05.24
+autor: Mikhail625@protonmail.com
+*/
+
+
+/*
+ * Tip: for formatting Ctrl + K, а затем Ctrl + D
+ * */
+
+
+
+using System;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
 using System.Data.SqlClient;
+
 
 namespace DBMan
 {
@@ -32,7 +43,9 @@ namespace DBMan
         private void button1_Click(object sender, EventArgs e)
         {
             //textbox_label();
-
+            // ConnMSSQLExpess();
+            
+            ConnTest1();
         }
 
         private void textbox_label()
@@ -106,8 +119,11 @@ namespace DBMan
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox11.Text = "local";
-            comboBox1.Text = "MS SQL";
+            MessageBox.Show(textBox6.Text);
+
+
+
+
         }
 
 
@@ -124,12 +140,43 @@ namespace DBMan
             var conn = new MySqlConnection(connString);
 
         }
+
+        private void ConnMSSQLExpess()
+        {
+
+            var server = "Data Source=ServerName";
+            var port = 1433;
+            var dataBase = "Initial Catalog=DataBaseName";
+
+
+            var userName = "User id=UserName";
+            var password = "Password=UserPassword";
+            var connString =
+            string.Join(";", new[] { server, dataBase, userName, password });
+
+            using (var conn = new SqlConnection(connString)) { conn.Open(); }
+        }
+
+
+        private void SetDefMySQL()
+        {
+            textBox11.Text = "local";
+            comboBox1.Text = "MySQL";
+            textBox10.Text = "127.0.0.1";
+            textBox9.Text = Convert.ToString(3306);
+            textBox8.Text = "persons";
+            textBox7.Text = "root";
+            textBox6.Text = "ac9b8f664f";
+
+
+        }
+
         private void ConnMSSQLAutSQL()
         {
 
 
-           // If you use SQL authentication, use this:
-           // using System.Data.SqlClient;
+            // If you use SQL authentication, use this:
+            // using System.Data.SqlClient;
 
             SqlConnection conn = new SqlConnection();
             conn.ConnectionString =
@@ -146,6 +193,51 @@ namespace DBMan
 
         }
 
+        private void ConnTest1()
+        {
+            /*
+            textBox11.Text = "local";
+            comboBox1.Text = "MySQL";
+            textBox10.Text = "127.0.0.1";
+            textBox9.Text = Convert.ToString(3306); // port
+            textBox8.Text = "persons";  // database name
+            textBox7.Text = "root"; // user name
+            textBox6.Text = "ac9b8f664f";  //pass
+            */
+            //
+
+            var host = textBox10.Text;
+            var port = textBox9.Text;
+            var dataBase = textBox8.Text;
+            var userName = textBox7.Text;
+            var password = textBox6.Text;
+            var connString = $"Server={host};Database={dataBase};port={port}" +
+                $";User Id={userName};password={password}";
+
+            listBox1.Items.Add(" " + host);
+            listBox1.Items.Add(" " + dataBase);
+            listBox1.Items.Add(":" + port);
+          //  listBox1.Items.Add(" " + dataBase);
+          //  listBox1.Items.Add(" " + dataBase);
+          //  listBox1.Items.Add(" " + dataBase);
+              
+            var conn = new MySqlConnection(connString);
+            conn.Open();
+            var command = conn.CreateCommand();
+            //  command.CommandText = @"SELECT Language, Percentage FROM countrylanguage WHERE " +
+            //                          @"CountryCode = 'RUS' ORDER BY Percentage DESC";
+            command.CommandText = @"SELECT * FROM tbl_name1";
+
+
+            var reader = command.ExecuteReader(); // Get the data
+            if (!reader.Read()) return;
+
+
+            Console.WriteLine(reader.GetString(0) + ": " + reader.GetFloat("Percentage"));
+            listBox1.Items.Add(reader.GetString(0) + ": " + reader.GetFloat("Percentage"));
+            listBox1.Update();
+
+        }
 
 
         private void ConnAutWin()
@@ -158,7 +250,7 @@ namespace DBMan
                  "User Instance=true;" +
                  "Integrated Security=true;" +
                  "AttachDbFilename=|DataDirectory|Database1.mdf;";
-conn.Open();
+            conn.Open();
 
         }
         private void ConnTest()
@@ -167,5 +259,50 @@ conn.Open();
 
         }
 
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            /*
+            MS SQL 
+            Access
+            MySQL
+            MariaDB
+            Oracle
+            PostgreSQL
+            MongoDB
+            FireBird
+            */
+            string choiceDB;
+            choiceDB = comboBox1.Text;
+
+
+
+            switch (choiceDB)
+            {
+                case "MS SQL":
+
+                    break;
+                case "MySQL":
+                    SetDefMySQL();
+
+
+                    break;
+                case "Access":
+
+                    break;
+                case "":
+
+                    break;
+            }
+
+
+
+
+        }
     } /// end of frmConn
 }
